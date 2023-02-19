fd77f67c3ded75597a8271b0246134b31effcc73
mxbrk <maximilian.bronkhorst@protonmail.com>
2023-02-19 15:04:44 +0100

    new comment

--- lib/const.dart
+++ lib/const.dart
@@ -1,3 +1,5 @@
+//This file contains general values for the application
 import 'package:flutter/material.dart';
 
+// setting the typical green VLOGG color
 const Color vlogGreen = Color(0xFF6aae22);
