92f87b65ae01c760c0e093bc450ed78c5e93aaea
mxbrk <maximilian.bronkhorst@protonmail.com>
2023-02-22 23:18:38 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,9 @@ import 'const.dart';
 import 'dart:io';
 import 'package:csv/csv.dart';
 import 'package:path_provider/path_provider.dart';
+
 // BRANCH TEST TEST
+//test mb
 //begin app
 void main() => runApp(const MyApp());
 
