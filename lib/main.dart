b8584de250e764e76f3fdfa98d86f9d323497937
pwdrummer <der_gruene@mailbox.org>
2023-02-22 16:19:41 +0100

    test if branch checkout works

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'const.dart';
 import 'dart:io';
 import 'package:csv/csv.dart';
 import 'package:path_provider/path_provider.dart';
-
+// BRANCH TEST TEST
 //begin app
 void main() => runApp(const MyApp());
 
